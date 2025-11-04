cf5ab686734fcab3eddcb55d61caff4d469fa554
Chu <chuduong2504@gmail.com>
2025-11-05 02:56:07 +0700

    fix src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,10 @@ import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* File này đã Cấu hình đúng! 
-      BrowserRouter và AuthProvider đã bọc App.
-    */}
-    <div className="min-h-screen ...">
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
