630eae75eddc2522a1843a3b41f76a6a256968a4
Binh Happy <binhb211@gmail.com>
2025-11-12 01:14:58 +0700

    feature<Feature/admin>:Sua layout dashboard

--- src/pages/admin/DashboardPage.jsx
+++ src/pages/admin/DashboardPage.jsx
@@ -63,7 +63,7 @@ export default function DashboardPage() {
       id: 2,
       title: "Review Sprint 2",
       time: "16:00 - 17:00",
-      room: "Phòng h",
+      room: "Phòng họp 3",
       organizer: "Duong",
     },
   ];
