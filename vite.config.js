6cb53f9ffe1b8c2c540d934320208c05fb7ebf79
KienDoTri <kiendotri@gmail.com>
2025-12-09 20:06:26 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  base: './', // 👈 thêm dòng này — rất quan trọng khi deploy qua Nginx
+  server: {
+    host: '0.0.0.0', // Cho phép truy cập từ ngoài VM
+    port: 5173,
+    strictPort: true,
+    cors: true,
+  },
 })
