4df647c3bcfc50a75aca0f3dce9f53e5d8f7f727
KienDoTri <kiendotri@gmail.com>
2025-12-09 23:03:43 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
   server:{host: '0.0.0.0', // Lắng nghe trên tất cả các địa chỉ IP
     port: 5173, // Chọn cổng muốn sử dụng
     strictPort: true, // Đảm bảo Vite không thay đổi cổng
