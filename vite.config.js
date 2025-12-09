7935eb087429fa7830c11938ea1a280fa305b343
KienDoTri <kiendotri@gmail.com>
2025-12-09 22:56:56 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: './', // 👈 thêm dòng này — rất quan trọng khi deploy qua Nginx
-  server: {
-    host: '0.0.0.0', // Cho phép truy cập từ ngoài VM
-    port: 5173,
-    strictPort: true,
+  plugins: [react(),tailwindcss()],
+  server:{host: '0.0.0.0', // Lắng nghe trên tất cả các địa chỉ IP
+    port: 5173, // Chọn cổng muốn sử dụng
+    strictPort: true, // Đảm bảo Vite không thay đổi cổng
     cors: true,
-  },
+    
+  }
 })
